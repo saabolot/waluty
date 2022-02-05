e765bee67654b4fa6cbfc3e1510ecc4e7220b70e
saabolot <97003103+saabolot@users.noreply.github.com>
2022-02-05 15:45:55 +0100

    Add files via upload

--- waluty.py
+++ waluty.py
@@ -1,3 +1,4 @@
+from optparse import Values
 import requests
 import csv
 
@@ -11,3 +12,22 @@ with open('waluty.csv', 'w', newline='') as output_file:
     fc = csv.DictWriter(output_file, delimiter =';' , fieldnames=column)
     fc.writeheader()
     fc.writerows(data2)
+
+
+kody=[]
+kursy=[]
+with open("waluty.csv", "r") as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=';')
+    for lines in csv_reader:
+        kody.append(lines['code'])
+        kursy.append(lines['bid'])
+
+waluty_dict = dict(zip(kody, kursy))
+print(kody)
+symbol = str(input('Wybierz z powyszych symbol waluty do przeliczenia: '))
+ilosc = float(input('Podaj ilość waluty do przeliczenia: '))
+
+if symbol in waluty_dict:
+    print(f"Wartość sprzedawanej waluty to: ", round((float(waluty_dict[symbol])*ilosc), 2))
+else:
+    print("nieprawidłowy symbol waluty")
